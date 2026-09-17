40bac7c201e2c804d114440fa71cc37bbf4f53b6
Kirill Ivashchenko <strategovich@gmail.com>
2026-09-18 01:05:20 +0300

    Баланс: стоимость генератора тёмных сфер 2^n -> 1.8^n

--- js/dark_matter.js
+++ js/dark_matter.js
@@ -1,6 +1,6 @@
 // Costs Dark Matter
 function getDarkOrbGeneratorCost() {
-    return new Decimal(2).pow(gameData.dark_matter_shop.dark_orb_generator)
+    return new Decimal(1.8).pow(gameData.dark_matter_shop.dark_orb_generator)
 }
 
 function isDecimalInfinity(value) {
